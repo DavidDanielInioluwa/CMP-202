3f2df94c352c53a5572e833a48faa9330c014aca
DavidDanielInioluwa <daniel.david@binghamuni.edu.ng>
2024-06-09 22:58:25 +0100

    Initial commit

---
+++ animal.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Person {
+
+    private String schoolName = "Bingham University"
+
+    private String name = "Jane Doe";
+
+    private String password;
+
+    string getName() {
+        return name;
+        
+    }
+
+}
